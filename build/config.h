e4a811110baf3d3738c87636606de9a66471bcf3
Davide Zuliani INFN Padova - PostDoc <zuliani@muonc-monster.novalocal>
2024-07-15 12:13:50 +0200

    removed build dir

--- build/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//! Location of CMake SOURCE directory (aka resource files before install)
-#define ACTSTRACKING_SOURCEDIR "/users/muoncollider/zuliani/ACTSTracking/"
-
-//! Installation location for data files
-#define ACTSTRACKING_DATADIR "/usr/local/share/ACTSTracking/"
