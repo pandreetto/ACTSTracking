460bdaf6b38a462dabd6d161f457646b9f8f8c8d
Davide Zuliani INFN Padova - PostDoc <zuliani@muonc-monster.novalocal>
2024-07-15 15:18:17 +0200

    fix VXD mapping

--- src/GeometryIdMappingTool.cxx
+++ src/GeometryIdMappingTool.cxx
@@ -22,7 +22,7 @@ const std::unordered_map<uint32_t, uint32_t>
 
 const std::unordered_map<uint32_t, uint32_t>
     GeometryIdMappingTool::NRng_VertexEndCap = {
-        {2, 16}, {4, 16}, {6, 16}};
+        {2, 16}, {4, 16}, {6, 16}, {8,16}};
 
 const std::unordered_map<uint32_t, uint32_t>
     GeometryIdMappingTool::NLad_InnerTrackerBarrel = {
